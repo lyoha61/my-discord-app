5dfadb9abe373e323430d7a44b5f32291ff31e80
lyoha61 <nightmessenger703@gmail.com>
2025-08-24 21:00:40 +0300

    feat(auth): implement AuthGuard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
@@ -12,6 +15,7 @@ import { ConfigService } from '@nestjs/config';
       })
     })
   ],
+  providers: [JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from "src/prisma/prisma.service";
+import { UserForToken } from "./interfaces/user-for-token.interface";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+	constructor(
+		private config: ConfigService, 
+		private prisma: PrismaService 
+	) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: false,
+			secretOrKey: config.getOrThrow<string>('JWT_SECRET')
+		})
+	}
+	
+	async validate(payload: { sub: number }): Promise<UserForToken | null> {
+		const user = await this.prisma.user.findUnique({ where: { id: payload.sub } });
+		if (!user) return null;
+		return { id: user.id };
+	}
+}
