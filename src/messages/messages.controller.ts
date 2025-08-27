7aaba5685f5f4e944e2f1fad3d13fce0901a3945
lyoha61 <nightmessenger703@gmail.com>
2025-08-27 11:33:24 +0300

    refactor(messages): move business logic from controller to service

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,29 +1,17 @@
-import {Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards, Logger, Get, Param, ParseIntPipe, Delete, NotFoundException, Patch } from '@nestjs/common';
+import {Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards, Logger, Get, Param, ParseIntPipe, Delete, Patch } from '@nestjs/common';
 import type { Request } from 'express';
 import CreateMessageDto from './dto/create-message.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
 import User from 'src/common/decorators/user.decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import UpdateMessageDto from './dto/update-message.dto';
-import { Message } from '@prisma/client';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 @UseGuards(JwtAuthGuard)
 export class MessagesController {
 	private readonly logger = new Logger(MessagesController.name);
 
-	constructor(private readonly prisma: PrismaService) {}
-
-	async findMessage(messageId: number, authorId: number): Promise<Message | null> {
-		const message = await this.prisma.message.findFirst({
-			where: {
-				id: messageId,
-				author_id: authorId
-			}
-		});
-		
-		return message;
-	}
+	constructor(private messagesService: MessagesService) {}
 
 	@Get('/:messageId')
 	async getMessage( 
@@ -31,38 +19,18 @@ export class MessagesController {
 		@User('id') userId: number,
 		@Param('messageId', ParseIntPipe) messageId: number
 	) {
-		try {
-			const message = await this.findMessage(messageId, userId);
-
-			if(!message) throw new NotFoundException(`Message with id: ${messageId} not found or you not athor`);
-
-			this.logger.log(`Пользователь с id: ${userId} запросил ресурс ${req.method} ${req.originalUrl} `);
-			return message;
-		} catch (err) {
-			this.logger.error(`Ошибка получения сообщения с id: ${messageId}`);
-			throw(err);
-		}
+		this.logger.log(`Пользователь с id: ${userId} запросил ресурс ${req.method} ${req.originalUrl} `);
+		const message = this.messagesService.getMessage(messageId, userId);
+		return message;
 	}
 
 	@Get()
 	async getMessages(@Req() req: Request, @User('id') userId: number) {
-		try {
-			const messages = await this.prisma.message.findMany({
-				where: {author_id: userId}
-			});
+		this.logger.log(`Пользователь с id: ${userId} запросил ресурс ${req.method} ${req.originalUrl}`)
 
-			if (messages.length === 0) {
-				return ({message: 'У пользователя отсутствуют сообщения'});
-			}
-			
-			this.logger.log(`Пользователь с id: ${userId} запросил ресурс ${req.method} ${req.originalUrl}`)
+		const messages = this.messagesService.getMessages();
 
-			return messages;
-
-		} catch (err) {
-			this.logger.error('Ошибка при получении сообщений');
-			throw err;
-		}
+		return messages;	
 	}
 
 
@@ -72,26 +40,8 @@ export class MessagesController {
 		@Body() body: CreateMessageDto, 
 		@User('id') userId: number
 	) {
-		try {
-			const message = await this.prisma.message.create({
-				data: {
-					text: body.text,
-					author: {
-						connect: {
-							id: userId,
-							
-						} 
-					}
-				}
-			});
-
-			this.logger.log(`Новое сообщение создано id: ${message.id}`);
-
-			return message;
-		} catch(err) {
-			this.logger.error('Ошибка при сохранении сообщения');
-			throw err;
-		}
+		const message = this.messagesService.storeMessage(body.text, userId);
+		return message;
 	}
 
 	@Patch('/:messageId')
@@ -100,28 +50,13 @@ export class MessagesController {
 		@User('id') userId: number,
 		@Param('messageId', ParseIntPipe) messageId: number
 	){
-		try {
-			const message = await this.findMessage(messageId, userId);
-			if(!message) throw new NotFoundException(`Message with id: ${messageId} not found or you not athor`);
+		const updatedMessage = this.messagesService.updateMessage(
+			body.text, 
+			messageId, 
+			userId
+		);
 
-			const newDataMessage = body.text;
-			const updatedMessage = await this.prisma.message.update({
-				where: {
-					id: messageId,
-					author_id: userId
-				},
-				data: {
-					text: newDataMessage
-				}
-			});
-
-			this.logger.log(`Message with id: ${messageId} updated`);
-
-			return updatedMessage;
-		} catch (err) {
-			this.logger.error(`Errro during update message with id: ${messageId}`);
-			throw err;
-		}
+		return updatedMessage;
 	}
 
 	@Delete('/:messageId')
@@ -129,23 +64,10 @@ export class MessagesController {
 		@User('id') userId: number,
 		@Param('messageId', ParseIntPipe) messageId: number
 	) {
-		try {
-			const result = await this.prisma.message.delete({
-				where: {
-					id: messageId,
-					author_id: userId
-				}
-			});
-			if(!result) throw new NotFoundException('Сообщение не найдено или вы не автор');
-
-			this.logger.log(`Сообщение успешно удалено id: ${messageId}`);
-			return {
-				message: 'Сообщение успешно удалено',
-				message_id: messageId
-			}
-		} catch (err) {
-			this.logger.error(`Ошибка при удалении сообщения id: ${messageId}`);
-			throw(err);
+		this.messagesService.destroyMessage(messageId, userId);
+		return {
+			status: 'success',
+			message_id: messageId
 		}
 	}
 }
---
+++ src/messages/messages.service.ts
@@ -0,0 +1,119 @@
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Message } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class MessagesService {
+	private readonly logger = new Logger(MessagesService.name);
+	constructor(private prisma: PrismaService) {}
+
+	protected async findMessage(messageId: number, authorId: number): Promise<Message | null> {
+		const message = await this.prisma.message.findFirst({
+			where: {
+				id: messageId,
+				author_id: authorId
+			}
+		});
+		
+		return message;
+	}
+
+	async getMessage(messageId: number, userId: number) {
+		try {
+			const message = await this.findMessage(messageId, userId);
+
+			if(!message) throw new NotFoundException(`Message with id: ${messageId} not found or you not athor`);
+
+			return message;
+		} catch (err) {
+			this.logger.error(`Ошибка получения сообщения с id: ${messageId}`);
+			throw(err);
+		}
+	}
+
+	async getMessages() {
+		try {
+			const messages = await this.prisma.message.findMany();
+
+			if (messages.length === 0) {
+				return ({ 
+					messages: []
+				});
+			}
+			return messages;
+		} catch(err) {
+			this.logger.error('Ошибка при получении сообщений');
+			throw(err);
+		}
+		
+	}
+
+	async storeMessage(text: string, userId: number) {
+		try {
+			const message = await this.prisma.message.create({
+				data: {
+					text: text,
+					author: {
+						connect: {
+							id: userId,
+							
+						} 
+					}
+				}
+			});
+
+			this.logger.log(`Новое сообщение создано id: ${message.id}`);
+
+			return message;
+		} catch(err) {
+			this.logger.error('Ошибка при сохранении сообщения');
+			throw(err);
+		}
+		
+	}
+
+	async updateMessage(text: string, messageId: number, userId: number) {
+		try {
+			const message = await this.findMessage(messageId, userId);
+			if(!message) throw new NotFoundException(`Message with id: ${messageId} not found or you not athor`);
+
+			const updatedMessage = await this.prisma.message.update({
+				where: {
+					id: messageId,
+					author_id: userId
+				},
+				data: {
+					text
+				}
+			});
+
+			this.logger.log(`Message with id: ${messageId} updated`);
+
+			return updatedMessage;
+		} catch (err) {
+			this.logger.error(`Errro during update message with id: ${messageId}`);
+			throw err;
+		}
+	}
+
+	async destroyMessage(messageId: number, userId: number) {
+		try {
+			const result = await this.prisma.message.delete({
+				where: {
+					id: messageId,
+					author_id: userId
+				}
+			});
+			if(!result) throw new NotFoundException('Сообщение не найдено или вы не автор');
+
+			this.logger.log(`Сообщение успешно удалено id: ${messageId}`);
+			return {
+				message: 'Сообщение успешно удалено',
+				message_id: messageId
+			}
+		} catch (err) {
+			this.logger.error(`Ошибка при удалении сообщения id: ${messageId}`);
+			throw err;
+		}
+	}
+}
