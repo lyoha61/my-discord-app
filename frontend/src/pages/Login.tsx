71ccdf54fa1485a7126d6c44a069a8749ef57757
lyoha61 <nightmessenger703@gmail.com>
2025-08-27 16:48:57 +0300

    chore: rename file and change imports

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { login } from "../services/auth";
+import { login } from "../services/authService";
 import { useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { register } from "../services/auth";
+import { register } from "../services/authService";
 
 export const Register: React.FC  = () => {
 	const [email, setEmail] = useState<string>('');
